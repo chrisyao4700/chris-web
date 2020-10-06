2a45ff0b3df2f985efdcbaece152e70248db8277
Yuan Yao <chris@Yuans-MacBook-Pro.local>
2020-10-06 16:54:58 -0700

    SAVE FOR COOKERY

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,36 @@ const express = require('express');
 const router = express.Router();
 const skillConn = require('../modules/skills/skills.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  let skillList = skillConn.getSkillList();
-  res.render('home/page_root', {
-    title: 'Chris Yao',
-    skillList: skillList
-  });
-});
+const cookeryConn = require('../service/cookery.conn');
 
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    let skillList = skillConn.getSkillList();
+    res.render('home/page_root', {
+        title: 'Chris Yao',
+        skillList: skillList
+    });
+});
+router.get('/cookery/count', async function (req, res, next) {
+
+    const categories = ['chinese', 'mexican', 'japanese', 'fast', 'pizza', 'seafood', 'thai', 'italian', 'korean', 'american', 'sushi bar'];
+    const payload = await cookeryConn.coreRequest('GET', ['count'], req.query, {}, {});
+    res.render('cookery/page_root', {
+        title: 'Cookery Map API',
+        type: 'count',
+        categories: categories,
+        payload: payload
+    })
+});
+router.get('/cookery/area', async function (req, res, next) {
+
+    const categories = ['chinese', 'mexican', 'japanese', 'fast', 'pizza', 'seafood', 'thai', 'italian', 'korean', 'american', 'sushi bar'];
+    const payload = await cookeryConn.coreRequest('GET', ['area'], req.query, {}, {});
+    res.render('cookery/page_root', {
+        title: 'Cookery Map API',
+        type: 'area',
+        categories: categories,
+        payload: payload
+    });
+});
 module.exports = router;
