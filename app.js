b409b58d55c7d10b294bd5f4dc2078f6f90e6dcc
Yuan Yao <chris@Yuans-MacBook-Pro.local>
2020-10-06 17:03:04 -0700

    SAVE FOR PRD

--- app.js
+++ app.js
@@ -15,16 +15,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-app.get('*', (req, res, next) => {
-    if (req.headers["x-forwarded-proto"] === "https") {
-        return next();
-    }
-    res.redirect("https://" + req.headers.host + req.url);
-});
+if (process.env.perform === 'PRD') {
+    app.get('*', (req, res, next) => {
+        if (req.headers["x-forwarded-proto"] === "https") {
+            return next();
+        }
+        res.redirect("https://" + req.headers.host + req.url);
+    });
+}
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/node_modules/bootstrap', express.static(__dirname + '/node_modules/bootstrap'));
@@ -36,19 +38,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
