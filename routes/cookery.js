ac94cb819de1b8544f028537c6a4f161c98d6e1d
Yuan Yao <chris@Yuans-MacBook-Pro.local>
2020-10-13 08:47:46 -0700

    SAVE FOR ADDING REVIEW

--- routes/cookery.js
+++ routes/cookery.js
@@ -14,11 +14,10 @@ router.get('/', async function (req, res, next) {
 router.get('/zipcode', async function (req, res, next) {
 
     try {
-        const categories = ['chinese', 'mexican', 'japanese', 'fast', 'pizza', 'seafood', 'thai', 'italian', 'korean', 'american', 'sushi bar'];
+        const categories = await requestCategoryList();
         const {category, zipcode} = req.query;
         const payload = await requestCookeryZipcode(category, zipcode);
         const {lat: ziplat, lng: ziplng, city} = await requestGoogleZip(zipcode);
-
         res.render('cookery/page_root', {
             title: 'Cookery Map API',
             search_type: 'zipcode',
@@ -29,23 +28,39 @@ router.get('/zipcode', async function (req, res, next) {
             payload: payload
         })
     } catch (e) {
-        // console.log(e);
+        next(e);
+    }
+});
+
+router.get('/zipdetail', async function (req, res, next) {
+
+    try {
+
+        const {zipcode, count} = req.query;
+        const payload = await requestZipcodeDetail(zipcode);
+
+
+        res.render('zipdetail/page_root', {
+            title: 'Zipcode Detail',
+            location_count: count,
+            payload: payload
+        });
+    } catch (e) {
+
         next(e);
     }
 });
 router.get('/area', async function (req, res, next) {
 
-    try{
-        const categories = ['chinese', 'mexican', 'japanese', 'fast', 'pizza', 'seafood', 'thai', 'italian', 'korean', 'american', 'sushi bar'];
+    try {
+        // const categories = ['chinese', 'mexican', 'japanese', 'fast', 'pizza', 'seafood', 'thai', 'italian', 'korean', 'american', 'sushi bar'];
+        const categories = await requestCategoryList();
         const {category, lat, lng, radius} = req.query;
         // console.log(req.query);
         const payload = await requestCookeryArea(category, lat, lng, radius);
 
 
-        const {lat: ziplat, lng: ziplng, city} = await requestGoogleLatlng(lat,lng);
-
-        console.log(`origin: (${lat},${lng})`);
-        console.log(`back: (${ziplat},${ziplng})`);
+        const {lat: ziplat, lng: ziplng, city} = await requestGoogleLatlng(lat, lng);
 
         res.render('cookery/page_root', {
             title: 'Cookery Map API',
@@ -56,12 +71,35 @@ router.get('/area', async function (req, res, next) {
             zipcity: city,
             payload: payload
         });
-    }catch (e) {
+    } catch (e) {
         next(e);
     }
 });
 
-async function requestCookeryZipcode(category = 'mexican', zipcode = '90017') {
+router.get('/reviews', async (req, res, next) => {
+    try {
+        const {identifier} = req.query;
+        const payload = await requestReviewWithIdentifier(identifier);
+
+        res.render('reviews/page_root', {
+            title: 'Cookery Map Reviews',
+            payload: payload
+        });
+
+    } catch (e) {
+        next(e);
+    }
+});
+
+async function requestReviewWithIdentifier(identifier) {
+    try {
+        return await cookeryConn.coreRequest('GET', ['review', 'all', identifier], {}, {}, {});
+    } catch (e) {
+        throw e;
+    }
+}
+
+async function requestCookeryZipcode(category = 'Mexican', zipcode = '89103') {
 
     try {
         return await cookeryConn.coreRequest('GET', ['count', 'zipcode'], {category, zipcode}, {}, {});
@@ -71,7 +109,7 @@ async function requestCookeryZipcode(category = 'mexican', zipcode = '90017') {
 }
 
 
-async function requestGoogleZip(zipcode = '91709') {
+async function requestGoogleZip(zipcode = '89103') {
     try {
         return await googleConn.findFormattedAddress(zipcode);
     } catch (e) {
@@ -79,18 +117,39 @@ async function requestGoogleZip(zipcode = '91709') {
     }
 }
 
-async function requestGoogleLatlng(lat = '34.134423', lng='-117.223234') {
+async function requestGoogleLatlng(lat = '34.134423', lng = '-117.223234') {
     try {
 
-        return await googleConn.findFormattedAddressWithLatlng(lat,lng);
+        return await googleConn.findFormattedAddressWithLatlng(lat, lng);
     } catch (e) {
         throw e;
     }
 }
-async function requestCookeryArea(category = 'mexican', lat = '35.334423', lng = '-117.223234', radius = 23) {
+
+async function requestCookeryArea(category = 'Mexican', lat = '35.334423', lng = '-117.223234', radius = 23) {
 
     try {
         return await cookeryConn.coreRequest('GET', ['count', 'area'], {category, lat, lng, radius}, {}, {});
+    } catch (e) {
+        throw e;
+    }
+}
+
+async function requestCategoryList() {
+    try {
+
+        const {categories} = await cookeryConn.coreRequest('GET', ['category', 'all'], {}, {}, {});
+
+        return categories.map(ele => ele.name);
+    } catch (e) {
+        throw e;
+    }
+}
+
+async function requestZipcodeDetail(zipcode) {
+    try {
+        return await cookeryConn.coreRequest('GET', ['zipcode', 'detail', zipcode], {}, {}, {});
+
     } catch (e) {
         throw e;
     }
