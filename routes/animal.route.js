056d711e19559b2bac10854dad140a3409dbeeb9
Chris Yao <chrisyao900908@gmail.com>
2021-05-11 22:55:34 -0700

    SA

--- routes/animal.route.js
+++ routes/animal.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const redct = require('../service/redirect.service');
-router.get('/', (req, res, next) => {
+router.get('*', (req, res, next) => {
     // if (!CLIENT_NAME) {
     //     return next('CLIENT NAME IS NOT SPECIFIED')
     // }
