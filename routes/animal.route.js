763a960410aa9f712abf54a582d33b3bc577dba8
Chris Yao <chrisyao900908@gmail.com>
2021-05-26 19:30:35 -0700

    save logger

--- routes/animal.route.js
+++ routes/animal.route.js
@@ -3,13 +3,10 @@ const router = express.Router();
 const path = require('path');
 
 router.get('*', (req, res, next) => {
-    // if (!CLIENT_NAME) {
-    //     return next('CLIENT NAME IS NOT SPECIFIED')
-    // }
-    // console.log('?');
-    // console.log(req._parsedUrl._raw);
+
     const file_path = `${__dirname}/../animal-split-frontend/build/index.html`;
-    // console.log(file_path);
+
+    console.log(path.resolve(file_path));
     res.sendFile(path.resolve(file_path));
 });
 
